22a81da66bf1ea7291e582eba534784efe4e54d0
Have Life <have_life@163.com>
2012-08-03 01:00:48 +0800

    basic about user

--- build/classes/sql/createSQL.sql
+++ build/classes/sql/createSQL.sql
@@ -3,6 +3,6 @@ CREATE TABLE `user` (
   `username` varchar(30) NOT NULL,
   `password` varchar(30) NOT NULL,
   `email` varchar(100) DEFAULT NULL,
-  `createtime` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `createtime` datetime DEFAULT NULL,
   PRIMARY KEY (`id`,`password`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8
